5c16bd672c0e5e442d3c5cca285cc653bbb6d48c
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-16 09:19:15 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
-/* A c program that prints using puts */
-
 #include <stdio.h>
+/* A c program that prints using puts */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
