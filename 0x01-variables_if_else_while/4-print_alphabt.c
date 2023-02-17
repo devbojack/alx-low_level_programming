5cab27ef3352d850bcd1f59dcac04c3c59bd4c47
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-17 08:33:05 -0800

    Alphabet soup without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * declaration and initializtion of myA
+ * start of while loop
+ * printing of the alphabet
+ * New line
+ * Return: Always 0
+ */
+int main(void)
+{
+	char myA = 'a';
+
+	while (myA <= 'z')
+	{
+		if (myA == 'e' || myA == 'q')
+			myA = myA + 1;
+		else 
+			putchar(myA);
+		myA++;
+	}
+	putchar('\n');
+	return (0);
+}
