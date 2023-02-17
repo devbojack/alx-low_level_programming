c4f78c4ac7239684abf91c64abb6027a7b8640c6
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-17 09:50:21 -0800

    Printing hexadecimal with putchar

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,9 +4,8 @@
  * main- Entry point
  * declaration of myStart
  * start of number for loop and initialization of myStart
- * printing 0 - 9
- * start of char for loop 
- * printing a - f
+ * printing 0-9
+ * printing a-f
  * New line
  * Return: Always 0
  */
@@ -14,6 +13,7 @@ int main(void)
 {
 	int myNumStart;
 	char myCharStart;
+
 	for (myNumStart = 0; myNumStart <= 9; myNumStart++)
 		putchar(myNumStart + '0');
 	for (myCharStart = 'a'; myCharStart <= 'f'; myCharStart++)
