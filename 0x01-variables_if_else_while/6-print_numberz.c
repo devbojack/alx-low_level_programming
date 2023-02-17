fbd497054c3e1aa9d0f53687b780fca3c46d1deb
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-17 09:12:25 -0800

    Printing numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * declaration of myStart
+ * start of for loop and initialization of myStart
+ * printing 0 - 9
+ * New line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int myStart;
+
+	for (myStart = 0; myStart < 10; myStart++)
+		putchar(myStart + '0');
+	putchar('\n');
+	return (0);
+}
