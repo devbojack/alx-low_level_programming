341440f1f2bd4444a3cc6ee50d93f00f9e48ae46
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-17 12:32:31 -0800

    Number combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * 0 - 99
+ * New line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int leftNumber;
+	int rightNumber;
+
+	for (leftNumber = 0; leftNumber <= 9; leftNumber++)
+	{
+		for (rightNumber = 0; rightNumber <= 9; rightNumber++)
+		{
+			if(rightNumber > leftNumber)
+			{
+				putchar(leftNumber + '0');
+				putchar(rightNumber + '0');
+				if (leftNumber == 8 && rightNumber == 9)
+					continue;
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			if (leftNumber == 9 && rightNumber == 9)
+				continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
