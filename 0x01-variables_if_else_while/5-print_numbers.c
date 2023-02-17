f06744662e2d1b756fad4cd65453b9a72f9ab816
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-17 08:49:34 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * declaration of myStart
+ * start of for loop and initialization of myStart
+ * printing 0 - 9
+ * New line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int myStart;
+	
+	for (myStart=0 ; myStart < 10; myStart++)
+		printf("%d",myStart);
+	putchar('\n');
+	return (0);
+}
+
