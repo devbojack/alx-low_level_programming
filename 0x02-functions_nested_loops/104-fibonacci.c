0ed8af7449ef2756528c4bbf761021c6e48b9f93
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-21 08:58:49 -0800

    100 fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the first 100 fibonacci numbers
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 98)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+
+		++i;
+	}
+
+	printf("\n");
+	return (0);
+}
