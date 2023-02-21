d08e206df38bbd5c0d6563aea1d4339ff8edfe1d
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-21 03:16:44 -0800

    Print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return void
+ */
+void print_alphabet_x10(void)
+{
+	char myStart;
+	int printOnce;
+
+	for (printOnce = 0; printOnce < 10; printOnce++)
+	{
+		for (myStart = 'a'; myStart <= 'z'; myStart++)
+		{
+			_putchar(myStart);
+		}
+	}
+	_putchar('\n');
+}
