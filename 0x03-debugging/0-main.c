d8ef15b407928d342653ceb03429039508c909e6
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-22 09:29:32 -0800

    First test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return- 0 always
-*/
+ * main - Enry point
+ *
+ * return - 0 always
+ */
 
 int main(void)
 {
