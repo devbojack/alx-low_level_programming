13372b3f78201b23e285b00afc5228464fab09cd
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 11:40:30 -0800

    Add 10th task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,7 +16,7 @@ void print_triangle(int size)
 	{
 		for (a = 0; a < size; a++)
 		{
-			for (b = size - a;b > 1; b--)
+			for (b = size - a; b > 1; b--)
 				_putchar(32);
 			for (c = 0; c <= a; c++)
 				_putchar(35);
