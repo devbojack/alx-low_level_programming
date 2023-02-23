3369ef58f9bf33dabb55d9fe9c5d6eb39e704beb
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 04:07:30 -0800

    Fix first task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,13 @@
 /**
  * main - entry point
  * @c - The number to be checked
- *
- * Return - 0 always
+ * Return - 1 is is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
 {
 	if((c >= 65) && (c <= 90))
 		return (1);
-	else
-		return (0);
+	
+	return (0);
 }
