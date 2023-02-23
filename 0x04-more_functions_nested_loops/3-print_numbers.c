2ec510bd577a547ae41d46e0919346dad489b835
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 05:46:59 -0800

    Add Fourth task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print 0 to 9
+ *
+ * Return: 0 to 9 numbers
+ */
+void print_most_numbers(void)
+{
+	int myStart;
+
+	for (myStart = 0; myStart <= 9; myStart++)
+		_putchar(myStart + '0');
+
+	_putchar('\n');
+}
