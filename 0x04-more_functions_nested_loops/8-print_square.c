133d65173cacc5b8d121bc8ab8d3127ee3771c3a
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 08:50:59 -0800

    Add 8th task

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a square
+ * @size: Size of the square
+ *
+ * Return: A square
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{			
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
