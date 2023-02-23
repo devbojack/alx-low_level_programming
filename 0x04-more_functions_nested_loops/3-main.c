4e51e46dcce2548ac77a4f40936e34602590f9ae
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 06:53:29 -0800

    Fix forth task

--- 0x04-more_functions_nested_loops/3-main.c
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -5,8 +5,9 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    print_numbers();
-    return (0);
+	print_numbers();
+	return (0);
 }
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,6 +5,7 @@
   *
   * Return: The numbers since 0 up to 9
   */
+
 void print_numbers(void)
 {
 	int x;
