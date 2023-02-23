47b89ad9dffb237289a5d54805286dcb560ec244
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 07:05:30 -0800

    Fix forth task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,16 +3,13 @@
 /**
   * print_numbers - Print the numbers since 0 up to 9
   *
-  * Return: The numbers since 0 up to 9
+  * Return: The numbers from 0 to 9
   */
 void print_numbers(void)
 {
 	int x;
 
 	for (x = 0; x <= 9; x++)
-	{
 		_putchar(x + '0');
-	}
-
 	_putchar('\n');
 }
