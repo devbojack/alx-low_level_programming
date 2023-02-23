546dfecb052a0ecfb3219f6ee5553422ec477735
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 07:59:49 -0800

    Add 6th task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - Print a line
+ *
+ * Return - A line
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x<=n; x++)
+		_putchar(95);
+
+	_putchar('\n');
+}
