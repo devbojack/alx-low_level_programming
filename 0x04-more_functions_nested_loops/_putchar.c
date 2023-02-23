0314e58fb81c474cdc7d1446b22ce5c2c80712c6
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-23 06:24:28 -0800

    Fix forth task

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * Write char c to stdout
+ *
+ * Return : 1 on success
+ * Return : -1 on error
+ */
+
+int _putchar(char myChar)
+{
+	return (write(1, &myChar, 1));
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
+_putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-_putchar(char c);
