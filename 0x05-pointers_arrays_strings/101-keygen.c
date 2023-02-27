6eb5a02827228b755163e73d99dc5ed43619a3af
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-27 13:54:00 -0800

    Add 101 password generator task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main -  Generae random password
+ *
+ * Return: Alwys 0
+ */
+int main(void)
+{
+	char password[84];
+	int index = 0;
+	int sum = 0;
+	int a1, a2;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+	password[index] = '\0';
+
+	if (sum != 2772)
+	{
+		a1 = (sum - 2772) / 2;
+		a2 = (sum - 2772) / 2;
+
+		if ((sum - 2772) % 2 != 0)
+			a1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + a1))
+			{
+				password[index] -= a1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + a2))
+			{
+				password[index] -= a2;
+				break;
+			}
+		}
+	}
+
+	printf("%s \n" , password);
+	return (0);
+}
