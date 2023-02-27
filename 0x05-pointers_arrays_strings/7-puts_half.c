239c5e1269cb0e46143a216812352e7ac6f963ae
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-27 11:21:42 -0800

    7 print half a string task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - Print half of a sring
+ * @str: String to print
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = len / 2; i <= len; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
