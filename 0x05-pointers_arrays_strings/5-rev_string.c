1a22a128e1baeec563162c0809059fea9bfc9d67
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-02-27 10:29:31 -0800

    Add 5 Rev String task

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse a string
+ * @s: String to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, temp;
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
