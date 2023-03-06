49dee0da3e299dfad2074790dca13a21d4bc3904
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-06 15:49:33 -0800

    Add task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard
+ * @a: array
+ * @8: array length
+ * 
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
+
+		_putchar('\n');
+	}
+}
