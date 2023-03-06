75c376c3bd73c5d233f1826058949acbabc7e129
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-06 15:23:06 -0800

    Add task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * strstr - Locates a substring
+ * @haystack: String to search into
+ * @needle - Substring to seaarch for
+ *
+ * Return: Pointer or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int x = 0, y = 0;
+
+	while (haystack[x])
+	{
+		while (needle[y])
+		{
+			if (haystack[x + y] != needle[y])
+				break;
+
+			y++;
+		}
+
+		if (needle[y] == '\0')
+		{
+			return (haystack + x);
+		}
+
+		x++;
+	}
+
+	return (0);
+}
