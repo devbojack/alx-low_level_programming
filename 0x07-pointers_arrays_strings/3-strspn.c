d0400f117a0ebc85f41799cf8d56e70d99690151
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-06 13:36:25 -0800

    Add task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets length of a prefix substring
+ * @s: string to search into
+ * @accept: substring prefix to search
+ *
+ * Return: Number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int sLen, aLen, x, y;
+	unsigned int byteNum = 0;
+
+
+	for (sLen = 0; s[sLen] != '\0'; sLen++)
+		sLen++;
+
+	for (aLen = 0; accept[aLen] != '\0'; aLen++)
+		aLen++;
+
+	for (x = 0; x <= aLen; x++)
+	{
+		for (y = 0; y <= sLen && s[y] != 32; y++)
+		{
+			if (accept[x] == s[y])
+				byteNum++;
+		}
+	}
+
+	return (byteNum);
+}
