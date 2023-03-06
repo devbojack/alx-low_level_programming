17d6d2242abc410d16773ce1bd71b82f6e5b5dd0
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-06 14:37:27 -0800

    Fix task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,25 +9,19 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int sLen, aLen, x, y;
-	unsigned int byteNum = 0;
+	unsigned int byteNum = 0, x = 0, y;
 
-	for (sLen = 0; s[sLen] != '\0'; sLen++)
-		sLen++;
-
-	for (aLen = 0; accept[aLen] != '\0'; aLen++)
-		aLen++;
-
-	for (x = 0; x <= aLen; x++)
+	while (accept[x])
 	{
-		for (y = 0; y <= sLen; y++)
+		y = 0;
+
+		while (s[y] != 32)
 		{
-			if (s[y] != 32)
-			{
-				if (accept[x] == s[y])
-					byteNum++;
-			}
+			if (accept[x] == s[y])
+				byteNum++;
+			y++;
 		}
+		x++;
 	}
 
 	return (byteNum);
