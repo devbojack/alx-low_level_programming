7a37cfe53370579478130087d4963be04fea6138
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-08 10:52:44 -0800

    Add task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+int _doubler(int n, int x);
+
+/**
+ * _sqrt_recursion - Returns square root
+ * @n: Value
+ *
+ * Return: 1 or square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (_doubler(n, 1));
+}
+
+/**
+ * _doubler - Doubles for square
+ * @n: Value for squareroot
+ * @i: Doubler
+ *
+ * Return: Square root or -1
+ */
+int _doubler(int n, int x)
+{
+	int doubler = x * x;
+
+	if (doubler == n)
+		return (x);
+
+	if (doubler > n)
+		return (-1);
+
+	return (_doubler(n, x + 1));
+}
