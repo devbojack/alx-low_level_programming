8c2185cb2608ccc4a7d8cea4dc7ddfa8416cbcc2
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-10 13:03:49 -0800

    Add task 100: Coins change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,25 +26,25 @@ int main(int argc, char *argv[])
 
 		if ((cents - 25) >= 0)
 		{
-			cents -= 25;
+			cents-=25;
 			continue;
 		}
 
 		if ((cents - 10) >= 0)
 		{
-			cents -= 10;
+			cents-=10;
 			continue;
 		}
 
 		if ((cents - 5) >= 0)
 		{
-			cents -= 5;
+			cents-=5;
 			continue;
 		}
 
 		if ((cents - 2) >= 0)
 		{
-			cents -= 2;
+			cents-=2;
 			continue;
 		}
 
