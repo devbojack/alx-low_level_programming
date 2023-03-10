22013834ff6266c075b7b4b50983dffa42f8d2e1
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-10 11:51:02 -0800

    Add task 0:  program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: Command lind argument count
+ * @argv: Command line array
+ *
+ * Return: Always zero
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
