e46317e65e30ca95388823a98cb6a6044b7bc2c3
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-22 03:37:06 -0700

    Fix task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,13 +1,13 @@
 #include "function_pointers.h"
 
-/*
-  * array_iterator - executes a function
-  * @array: array
-  * @size: size of array
-  * @action: function to use
-  *
-  * Return: void
-  */
+/**
+ * array_iterator - executes a function
+ * @array: array
+ * @size: size of array
+ * @action: function to use
+ *
+ * Return: void
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
