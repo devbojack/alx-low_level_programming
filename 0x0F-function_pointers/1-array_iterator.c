aa926ef141bdf285b105391cd4e6a50285efffaa
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-22 03:32:11 -0700

    Add task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/*
+  * array_iterator - executes a function
+  * @array: array
+  * @size: size of array
+  * @action: function to use
+  *
+  * Return: void
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
+	{
+		while (i < size)
+		{
+			action(array[i]);
+			i++;
+		}
+	}
+}
