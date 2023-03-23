26071557c0006cff55fdbcba82df2fb35a522557
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-23 07:45:36 -0700

    Add task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Returns sum
+ * @n: params
+ *
+ * Return: sum or 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list params;
+	unsigned int i = 0;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(params, n);
+
+	for (;i < n; i++)
+	{
+		sum += va_arg(params, int);
+	}
+
+	va_end(params);
+
+	return (sum);
+}
