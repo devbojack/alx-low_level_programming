020f4cf49fb40a30cd57b3a3d6a7b2329f37aca2
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-23 09:06:37 -0700

    Add task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings
+ * @separator: String btwn
+ * @n: Params size
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list params;
+	unsigned int x = 0;
+	char* stringarr;
+
+	va_start(params, n);
+
+	if (n > 0)
+	{
+		va_start(params, n);
+
+		for (; x < n; x++)
+		{
+			stringarr = va_arg(params, char *);
+			if (stringarr == NULL)
+				printf("%s", "(nil)");
+			else
+				printf("%s", stringarr);
+
+			if (x < (n - 1) && separator != NULL)
+				printf("%s", separator);
+		}
+
+		va_end(params);
+	}
+
+	printf("\n");
+}
