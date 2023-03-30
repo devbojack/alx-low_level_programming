4d84f6c53ac626dd0a18a4d81731613ffe665b77
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-03-30 08:15:32 -0700

    Task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - Prints all elemments of a list
+ * @h: List pointer
+ *
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *x = h;
+	size_t count = 0;
+
+	while (x != NULL)
+	{
+		if ((*x).str != NULL)
+			printf("[%d] %s\n", (*x).len,x->str);
+		else
+			printf("[0] (nil)\n");
+		count++;
+		x = x->next;
+	}
+
+	return (count);
+}
