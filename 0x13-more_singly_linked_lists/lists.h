52d4e67fddd0ab50fa6cd86a30a2e627cc194530
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-03 12:00:53 -0700

    Add task 2

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,19 +5,21 @@
 #include <stdio.h>
 #include <string.h>
 
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * error
+ * 
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
