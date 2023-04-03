cfe405f98f243d8a848fd74bef46befc3634f177
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-03 11:03:25 -0700

    Fix lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -16,9 +16,12 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-} listint_t;
+}
+listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+
+
 
 #endif
