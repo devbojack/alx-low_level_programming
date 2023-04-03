a6dbd3338329aef0484439c0fe2bc746d57c2e80
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-03 12:33:46 -0700

    Fix task3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,13 +20,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp->next = NULL;
 
 	if (ptr == NULL)
+	{
 		*head = temp;
+	}
 	else
 	{
 		while (ptr->next != NULL)
-		{
 			ptr = ptr->next;
-		}
+
 		ptr->next = temp;
 	}
 
