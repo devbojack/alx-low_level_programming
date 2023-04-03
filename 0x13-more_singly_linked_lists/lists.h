85603f5400b88378b64ce950799498811c68d229
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-03 11:09:39 -0700

    Fix task 1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,8 +10,7 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * 
+ * Description: singly linked list node structure 
  */
 typedef struct listint_s
 {
