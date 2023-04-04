bac1bb060be049c338229c5fa13bde43286d77af
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-04 01:05:00 -0700

    Add task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,17 +19,15 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	temp->n = n;
 	temp->next = NULL;
 
-	if (ptr == NULL)
-	{
-		*head = temp;
-	}
-	else
+	if (ptr != NULL)
 	{
 		while (ptr->next != NULL)
+		{
 			ptr = ptr->next;
-
+		}
 		ptr->next = temp;
 	}
-
+	else
+		*head = temp;
 	return (temp);
 }
