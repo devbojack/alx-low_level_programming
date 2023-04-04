960dbab040113e950377d82af0fc82511a2135f8
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-04 06:24:02 -0700

    Add task 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Delete idx node
+ * @head: head list pointer
+ * @index: Index to delete
+ *
+ * Return: 1 or -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int count = 1;
+	listint_t *current = *head;
+	listint_t *previous = *head;
+
+	if ((*head == NULL) || (head == NULL))
+	{
+		return (-1);
+	}
+	else if (index == 1)
+	{
+		*head = current->next;
+		free(current);
+		current = NULL;
+		return (1);
+	}
+	else
+	{
+		if (index > 1)
+		{
+			while (current)
+			{
+				if (index == count)
+				{
+					previous->next = current->next;
+					free(current);
+					return (1);
+				}
+				previous = current;
+				current = current->next;
+				count++;
+			}
+			if (index > count)
+				return (-1);
+		}
+	}
+	return (-1);
+}
