7c756fba6d9c9f9ab82f871d466c10ed914e9bd6
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-04 05:20:39 -0700

    Fix null return

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,23 +10,21 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
+	unsigned int count = 1;
 	listint_t *ptr, *ptr2;
 
 	if (head != NULL)
 	{
-		ptr = *head;
 		ptr2 = malloc(sizeof(listint_t));
 
 		if (ptr2 == NULL)
 			return (NULL);
 
 		ptr2->n = n;
-		ptr2->next = NULL;
 	
 		if (idx > 0)
 		{
-			idx--;
+			ptr = *head;
 			while (ptr)
 			{
 				if (count == idx)
@@ -41,6 +39,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (idx > count)
 				return (NULL);
 		}
+		else
+		{
+			ptr2->next = *head;
+			*head = ptr2;
+		}
+		return (ptr2);
 	}
 
 	return (NULL);
