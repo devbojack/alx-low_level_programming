076b72b01dd7e928c583b9e88135e9f9cb8d93c4
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-10 05:36:35 -0700

    Add task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  * set_bit - Sets value of a bit to 1 at index
-  * @n: rEP NUMBER
-  * @index: Given index
-  *
-  * Return: 1 or -1
-  */
+ * set_bit - Sets value of a bit to 1 at index
+ * @n: Represting number
+ * @index: Given index
+ *
+ * Return: 1 or -1
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int size = (sizeof(unsigned long int) * 8);
