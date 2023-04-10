8e53a65d4efe220ee096a83f409151cb07f3057e
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-10 05:20:54 -0700

    Add task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define _DEFINE
 
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -11,6 +11,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
 
 #endif
