55adda86b550621ba80f4a0394ccfa965b72f1b1
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-10 05:02:14 -0700

    Add task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * get_bet - returns the value of a bit at index
- * @n: Rep umber
+ * get_bit - returns the value of a bit at index
+ * @n: Rep number
  * @index: Given index
  *
  * Return: Int or -1
@@ -33,5 +33,6 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		return (0);
 	}
+
 	return (-1);
 }
