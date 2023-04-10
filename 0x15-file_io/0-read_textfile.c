7a6359cc3b6022533e576b9681f7c2bf93d51a6b
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-10 09:50:44 -0700

    Add task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+  * read_textfile - reads a text file and prints it
+  * @filename: File to read
+  * @letters: Size to print
+  *
+  * Return: size_t or 0
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int counter, size;
+	char *filer = malloc(sizeof(char *) * letters);
+
+	if (filer == NULL)
+	{
+		return (0);
+	}
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	
+	size = open(filename, O_RDONLY, 0600);
+
+	if (size == -1)
+	{
+		return (0);
+	}
+
+	counter = read(size, filer, letters);
+	write(STDOUT_FILENO, filer, counter);
+
+	free(filer);
+	close(size);
+	return (counter);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef _DEFINE
+#define _DEFINE
+
+#include <stdlib.h>
+#include <sys/uio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
