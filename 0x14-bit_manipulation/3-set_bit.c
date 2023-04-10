44d83b8a98b3d1e76f9d5348ac2227ea7823664c
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-10 05:19:24 -0700

    Add task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * set_bit - Sets value of a bit to 1 at index
+  * @n: rEP NUMBER
+  * @index: Given index
+  *
+  * Return: 1 or -1
+  */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int size = (sizeof(unsigned long int) * 8);
+	if (index > size)
+		return (-1);
+
+	if (*n |= 1 << index)
+		return (1);
+	else
+		return (-1);
+}
