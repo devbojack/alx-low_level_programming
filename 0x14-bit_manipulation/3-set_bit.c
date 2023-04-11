780c8ece4c247ac69fbdda7962d59d85c9310196
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-11 12:09:53 -0700

    Add task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -14,8 +14,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index > size)
 		return (-1);
 
-	if (*n |= 1 << index)
-		return (1);
-
-	return (-1);
+	return ((*n |= 1 << index) ? 1 : -1);
 }
