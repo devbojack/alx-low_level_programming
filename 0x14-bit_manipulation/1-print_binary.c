9ec3ec94196a85522d9fe808a426f903fbed816a
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-11 11:30:17 -0700

    Fix task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-void oneorzero(unsigned long int number);
-
 /**
  * print_binary - prints the binary rep
- * @n: represnting numbeer
+ * @n: representing number
  *
- * Return: void
+ * Return: void/ nothing
  */
 void print_binary(unsigned long int n)
 {
@@ -15,30 +13,28 @@ void print_binary(unsigned long int n)
 		_putchar('0');
 		return;
 	}
-
 	oneorzero(n);
 }
-
 
 /**
  * oneorzero - factorial to get 1 or 0
  * @number: representing number
  *
- * Return: void
+ * Return: void/ nothing
  */
 void oneorzero(unsigned long int number)
 {
-        if (number < 1)
-                return;
+	if (number < 1)
+		return;
 
 	oneorzero(number >> 1);
 
-        if (number & 1)
+	if (number & 1)
 	{
-                _putchar('1');
+		_putchar('1');
 	}
         else
 	{
-                _putchar('0');
+		_putchar('0');
 	}
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,6 +7,7 @@
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+void oneorzero(unsigned long int number);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
