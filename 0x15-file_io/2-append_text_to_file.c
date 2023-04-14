66bef4e0c0320980ba18128b5d623feba4e10dd3
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-04-14 07:17:02 -0700

    Fix task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,6 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	size = open(filename, O_WRONLY | O_APPEND);
+
 	if (size == -1)
 	{
 		return (-1);
@@ -29,12 +30,11 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		if (write(size, text_content, counter == -1))
+		if (write(size, text_content, counter) == -1)
 		{
 			return (-1);
 		}
 	}
-
 	close(size);
 	return (1);
 }
