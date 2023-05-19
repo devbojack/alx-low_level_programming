768aca7134df1046f14b63a5764f4be1acaffdd7
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-05-19 05:20:08 -0700

    6: Sum of the DLL

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Sum of all data in DLL
+ * @head: The head of the doubly linked list
+ *
+ * Return: The sum of all data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
+	int sum = 0;
+
+	if (head)
+	{
+		while (current != NULL)
+		{
+			sum += current->n;
+			current = current->next;
+		}
+	}
+
+	return (sum);
+}
