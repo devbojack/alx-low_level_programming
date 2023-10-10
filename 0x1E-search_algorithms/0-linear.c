f4ede50c374ded116d871973cce11a15215a4a6c
Justus Lolwerikoi <dev.bojack@gmail.com>
2023-10-10 14:35:27 -0700

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,24 +11,17 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
+
 	if (array != NULL)
 	{
-		int found_index = -1;
-		int is_found = 0;
-		size_t i = 0;
-
 		for (; i < size; i++)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
-			if (array[i] == value && !is_found)
-			{
-				is_found = 1;
-				found_index = i;
-			}
-		}
-
-		return (found_index);
+			if (array[i] == value)
+				return (i);
+		}	
 	}
 
 	return (-1);
